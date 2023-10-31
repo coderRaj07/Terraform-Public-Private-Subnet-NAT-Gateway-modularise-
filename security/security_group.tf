f3c1205b9cd116961ddc228271a62beab7af351a
Rajendra Bisoi <90323977+coderRaj07@users.noreply.github.com>
2023-10-31 22:06:29 +0530

    FEAT_0: security_group.tf

---
+++ security/security_group.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "my_security_group" {
+  name        = "my-security-group"
+  description = "Allow incoming SSH and outgoing traffic"
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
